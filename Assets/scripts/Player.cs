ac74801235246fec882e57748c6b90134be27732
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-09-30 09:54:11 -0300

    começada refatoração da classe player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,53 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
-    Transform playerTransform;
-    public float speed = 5f;
-    public int noise = 0;
+    
+    public float speed = 5f;    /* VELOCIDADE DE MOVIMENTO DO PLAYER */
+    
+    public float health = 100f; /* VIDA DO PLAYER */
+    
+    public float stamina = 100f;    /* STAMINA DO PLAYER */
 
-    Animator animator;
-    // Start is called before the first frame update
+    bool isWalking = false;     /* GUARDA SE O PLAYER ESTÁ ANDANDO */
+
+    Animator animator;      /* ANIMATOR PARA CONTROLAR AS ANIMAÇÕES DO PLAYER */
+
+    private AudioSource andar;      /* GUARDA O SOM DE ANDAR */
+
     void Start()
     {
+        andar = GetComponent<AudioSource>();
+        andar.loop = true;
         animator = GetComponent<Animator>();
-        playerTransform = GameObject.Find("Player").transform;
-        Debug.Log("Teste");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player é controlado pelo jogador
+        walk();
+    }
+
+    public void walk()  /* FAZ O JOGADOR ANDAR PELO MAPA AO PRESSIONAR OS BOTÕES */
+    {
+        /* ANDANDO HORIZONTALMENTE */
         if (Input.GetAxis("Horizontal") != 0)
         {
-            Vector3 newPosition = new Vector3(playerTransform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime, playerTransform.position.y, playerTransform.position.z);
-            playerTransform.position = newPosition;
+            if (isWalking == false)
+            {
+                andar.Play();
+            }
 
-            /* FLIPPING CHARACTER  */
+            this.isWalking = true;
+            Vector3 newPosition = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime, transform.position.y, transform.position.z);
+            transform.position = newPosition;
 
-            Vector3 scale = playerTransform.localScale;
+            /* VIRANDO O PERSONAGEM DE LADO  */
+
+            Vector3 scale = transform.localScale;
             if (Input.GetAxis("Horizontal") > 0)
             {
-                scale.x = Mathf.Abs(playerTransform.localScale.x);
+                scale.x = Mathf.Abs(transform.localScale.x);
             }
             else
             {
-                scale.x = -Mathf.Abs(playerTransform.localScale.x);
+                scale.x = -Mathf.Abs(transform.localScale.x);
             }
-            playerTransform.localScale = scale;
+            transform.localScale = scale;
             animator.SetBool("isWalking", true);
         }
+
+        /* ANDANDO VERTICALMENTE */
         if (Input.GetAxis("Vertical") != 0)
         {
-            Vector3 newPosition = new Vector3(playerTransform.position.x, playerTransform.position.y + Input.GetAxis("Vertical") * speed * Time.deltaTime, playerTransform.position.z);
-            playerTransform.position = newPosition;
+            if (isWalking == false)
+            {
+                andar.Play();
+            }
+            this.isWalking = true;
+            Vector3 newPosition = new Vector3(transform.position.x, transform.position.y + Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
+            transform.position = newPosition;
             animator.SetBool("isWalking", true);
         }
         if (!Input.GetButton("Vertical") && !Input.GetButton("Horizontal"))
         {
+            andar.Stop();
+            this.isWalking = false;
             animator.SetBool("isWalking", false);
         }
     }
